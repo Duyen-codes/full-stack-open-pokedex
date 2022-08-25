1f8e795c735472bcaaf4ad6e746a94d8231e9a4a
Duyen Nguyen <hongduyen0705@gmail.com>
2022-08-25 19:03:20 +0300

    fix eslint and console error

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: ["react", "jest"],
   rules: {
     indent: ["error", 2],
-    "no-console": "off",
+    "no-console": 0,
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
     semi: ["error", "never"],
